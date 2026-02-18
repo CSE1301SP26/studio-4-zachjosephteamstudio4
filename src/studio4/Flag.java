5e584f5ffdec5cbccdcd6c84186bfbbdba1fd38b
josephgabel <gabel@wustl.edu>
2026-02-18 15:19:56 -0600

    finished studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,18 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	double y = 0.03;
+	while (y < 0.93) {
+	StdDraw.line(0.05, y, 0.95, y);
+	y = y + 0.1;
+	}
+	StdDraw.line(0.05,0.03,0.05,0.93);
+	StdDraw.line(0.95,0.03,0.95,0.93);
+	StdDraw.setPenColor(0,250,0);
+		double trianglex[] = {0.4, 0.5, 0.6};
+		double triangley[] = {0.4, 0.6, 0.4};
+	 StdDraw.filledPolygon(trianglex, triangley);
+	StdDraw.setPenColor(250,0,0);
+	StdDraw.filledEllipse(0.5, 0.467, 0.05, 0.05);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,37 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String name = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		StdDraw.setPenColor(red,green,blue);
+		if (name.equals("rectangle")) {
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfwidth = in.nextDouble();
+		double halfheight = in.nextDouble();
+		if (filled == true) {
+		StdDraw.filledRectangle(x, y, halfwidth, halfheight);
+		}
+		else { StdDraw.rectangle(x, y, halfwidth, halfheight);
+		}
+		}
+		else if (name.equals("triangle")) {
+		double trianglex[] = new double[3];
+		double triangley[] = new double[3];
+		trianglex[0] = in.nextDouble();
+		triangley[0] = in.nextDouble();
+		trianglex[1] = in.nextDouble();
+		triangley[1] = in.nextDouble();
+		trianglex[2] = in.nextDouble();
+		triangley[2] = in.nextDouble();
+	if (filled == false) {
+		StdDraw.polygon(trianglex, triangley );
+		}
+		else { StdDraw.filledPolygon(trianglex, triangley );
+		}
+		}
 	}
 }
